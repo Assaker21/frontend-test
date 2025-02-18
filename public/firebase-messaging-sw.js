8f50e677cfb014b8b60f16828f7208059b69ce8b
Charbel Assaker <charbelassaker@live.com>
2025-02-18 14:31:56 +0200

    asf

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,29 +4,33 @@ importScripts(
   "https://www.gstatic.com/firebasejs/10.5.2/firebase-messaging.js"
 );
 
-// Initialize Firebase inside the Service Worker
-firebase.initializeApp({
-  apiKey: "AIzaSyBdtxWlWG5fyvPQGhAIX2b-D2h4tvuaaSk",
+try {
+  // Initialize Firebase inside the Service Worker
+  firebase.initializeApp({
+    apiKey: "AIzaSyBdtxWlWG5fyvPQGhAIX2b-D2h4tvuaaSk",
 
-  authDomain: "test-notifications-6b549.firebaseapp.com",
+    authDomain: "test-notifications-6b549.firebaseapp.com",
 
-  projectId: "test-notifications-6b549",
+    projectId: "test-notifications-6b549",
 
-  storageBucket: "test-notifications-6b549.firebasestorage.app",
+    storageBucket: "test-notifications-6b549.firebasestorage.app",
 
-  messagingSenderId: "215028577348",
+    messagingSenderId: "215028577348",
 
-  appId: "1:215028577348:web:5934f88f701cf5f81fb811",
-});
-
-// Get Messaging Instance
-const messaging = firebase.messaging();
-
-// Handle background notifications
-messaging.onBackgroundMessage((payload) => {
-  console.log("Received background message: ", payload);
-  self.registration.showNotification(payload.notification.title, {
-    body: payload.notification.body,
-    icon: payload.notification.icon || "/firebase-logo.png",
+    appId: "1:215028577348:web:5934f88f701cf5f81fb811",
   });
-});
+
+  // Get Messaging Instance
+  const messaging = firebase.messaging();
+
+  // Handle background notifications
+  messaging.onBackgroundMessage((payload) => {
+    console.log("Received background message: ", payload);
+    self.registration.showNotification(payload.notification.title, {
+      body: payload.notification.body,
+      icon: payload.notification.icon || "/firebase-logo.png",
+    });
+  });
+} catch (err) {
+  console.log("ERROR IN SW: ", err);
+}
